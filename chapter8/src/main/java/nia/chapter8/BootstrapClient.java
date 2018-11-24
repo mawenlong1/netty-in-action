35a2209c8a1833239799758e9d37e370664f5458
mawenlong <15061886113@163.com>
2018-11-24 18:39:26 +0800

    Netty实战

--- chapter8/src/main/java/nia/chapter8/BootstrapClient.java
+++ chapter8/src/main/java/nia/chapter8/BootstrapClient.java
@@ -3,12 +3,12 @@ package nia.chapter8;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
 import java.net.InetSocketAddress;
 
 /**
@@ -19,38 +19,36 @@ import java.net.InetSocketAddress;
  */
 public class BootstrapClient {
 
-  /**
-   * 代码清单 8-1 引导一个客户端
-   */
-  public void bootstrap() {
-    EventLoopGroup group = new NioEventLoopGroup();
-    Bootstrap bootstrap = new Bootstrap();
-    bootstrap.group(group)
-        .channel(NioSocketChannel.class)
-        .handler(new SimpleChannelInboundHandler<ByteBuf>() {
-          @Override
-          protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-            System.out.println("Received date");
-          }
-        });
-    //连接到远程主机
-    ChannelFuture future = bootstrap.connect(new InetSocketAddress("localhost", 8080));
-    future.addListener(new ChannelFutureListener() {
-      @Override
-      public void operationComplete(ChannelFuture future) throws Exception {
-        if (future.isSuccess()) {
-          System.out.println("connection established");
-        } else {
-          System.out.println("connection attempt failed");
-          future.cause().printStackTrace();
-        }
-      }
-    });
-  }
+    /**
+     * 代码清单 8-1 引导一个客户端
+     */
+    public void bootstrap() {
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .channel(NioSocketChannel.class)
+                .handler(new SimpleChannelInboundHandler<ByteBuf>() {
+                    @Override
+                    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
+                        System.out.println("Received date");
+                    }
+                });
+        //连接到远程主机
+        ChannelFuture future1 = bootstrap.connect(new InetSocketAddress("localhost", 8080));
+        future1.addListener((future) -> {
+            if (future.isSuccess()) {
+                System.out.println("connection established");
+            } else {
+                System.out.println("connection attempt failed");
+                future.cause().printStackTrace();
+            }
 
-  public static void main(String[] args) {
-    BootstrapClient client = new BootstrapClient();
-    client.bootstrap();
-  }
+        });
+    }
+
+    public static void main(String[] args) {
+        BootstrapClient client = new BootstrapClient();
+        client.bootstrap();
+    }
 
 }
